fc041f35750a275a61c275aecea02536462cd435
keanu-a <keanucaloua@gmail.com>
2025-03-31 18:26:59 -0700

    Matched Word and Phrase services

--- src/main/java/org/alouastudios/easytagalogbackend/mapper/PhraseMapper.java
+++ src/main/java/org/alouastudios/easytagalogbackend/mapper/PhraseMapper.java
@@ -1,16 +1,19 @@
 package org.alouastudios.easytagalogbackend.mapper;
 
+import org.alouastudios.easytagalogbackend.dto.phrase.PhraseRequestDTO;
 import org.alouastudios.easytagalogbackend.dto.phrase.PhraseResponseDTO;
 import org.alouastudios.easytagalogbackend.dto.phrase.PhraseWordResponseDTO;
 import org.alouastudios.easytagalogbackend.model.phrases.Phrase;
+import org.alouastudios.easytagalogbackend.model.phrases.PhraseWord;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class PhraseMapper {
 
-    // This maps a Phrase entity to the PhraseResponseDTO
+    // Maps a Phrase entity to the PhraseResponseDTO
     public PhraseResponseDTO toResponseDTO(Phrase phrase) {
-
         return new PhraseResponseDTO(
                 phrase.getUuid(),
                 phrase.getTagalog(),
@@ -23,5 +26,16 @@ public class PhraseMapper {
                         pw.getIsProperNoun()
                 )).toList()
         );
+    }
+
+    // Maps phrase request to Phrase entity object
+    public void toEntity(
+            Phrase phrase,
+            PhraseRequestDTO phraseRequestDTO,
+            List<PhraseWord> phraseWords) {
+        phrase.setTagalog(phraseRequestDTO.tagalog());
+        phrase.setEnglish(phraseRequestDTO.english());
+        phrase.setIsQuestion(phraseRequestDTO.isQuestion());
+        phrase.setPhraseWords(phraseWords);
     }
 }
