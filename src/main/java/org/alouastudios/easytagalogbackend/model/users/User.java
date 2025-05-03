217f85eceb4ea16a33b8026feeb717a065c0bca7
keanu-a <keanucaloua@gmail.com>
2025-05-02 23:22:56 -0700

    Created User entity

---
+++ src/main/java/org/alouastudios/easytagalogbackend/model/users/User.java
@@ -0,0 +1,42 @@
+package org.alouastudios.easytagalogbackend.model.users;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private UUID authId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    private int level = 1;
+    private int exp = 0;
+    private int streak = 0;
+
+    private LocalDate lastActiveDate;
+}
