ecc400c8db200e9a82a29c6cdbb511f53d16b71d
Keanu C <keanucaloua@gmail.com>
2025-05-23 22:26:41 -0700

    Created S3Signed service and logic

---
+++ src/main/java/org/alouastudios/easytagalogbackend/controller/AudioController.java
@@ -0,0 +1,25 @@
+package org.alouastudios.easytagalogbackend.controller;
+
+import org.alouastudios.easytagalogbackend.service.S3SignedUrlService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/audio")
+public class AudioController {
+
+    private final S3SignedUrlService s3SignedUrlService;
+
+    public AudioController(S3SignedUrlService s3SignedUrlService) {
+        this.s3SignedUrlService = s3SignedUrlService;
+    }
+
+    @GetMapping("/{audioUrl}")
+    public ResponseEntity<String> getSignedAudioUrl(@PathVariable String audioUrl) {
+        String signedUrl = s3SignedUrlService.generatePresignedUrl(audioUrl);
+        return ResponseEntity.ok(signedUrl);
+    }
+}
---
+++ src/main/java/org/alouastudios/easytagalogbackend/service/S3SignedUrlService.java
@@ -0,0 +1,38 @@
+package org.alouastudios.easytagalogbackend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class S3SignedUrlService {
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    public String generatePresignedUrl(String filePath) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(filePath)
+                .build();
+
+        GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        PresignedGetObjectRequest presignedGetObjectRequest = s3Presigner.presignGetObject(getObjectPresignRequest);
+
+        return presignedGetObjectRequest.url().toExternalForm();
+    }
+}
