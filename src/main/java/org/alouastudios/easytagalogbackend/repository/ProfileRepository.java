70d2ff63a9d012ea906b60ee7ca0202a7f61ee3e
keanu-a <keanucaloua@gmail.com>
2026-02-10 19:17:14 -0800

    feat: make UUID the only primary id in profiles

--- src/main/java/org/alouastudios/easytagalogbackend/repository/ProfileRepository.java
+++ src/main/java/org/alouastudios/easytagalogbackend/repository/ProfileRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
-    Optional<Profile> findByAuthId(UUID authId);
+    Optional<Profile> findById(UUID id);
 }
